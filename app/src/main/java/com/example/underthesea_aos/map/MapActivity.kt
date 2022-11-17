e5309c2998c43c873a71aeb7c814fce9e9d075a9
wyoung163 <20201014@sungshin.ac.kr>
2022-11-17 11:43:34 +0900

    feat: add place database using dbHelper

--- app/src/main/java/com/example/underthesea_aos/map/MapActivity.kt
+++ app/src/main/java/com/example/underthesea_aos/map/MapActivity.kt
@@ -1,9 +1,11 @@
 package com.example.underthesea_aos.map
 
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Transformations.map
 import com.example.underthesea_aos.R
+import com.example.underthesea_aos.databinding.ActivityMainBinding
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -19,10 +21,29 @@ class MainActivity : AppCompatActivity() {
     }
 }
 */
-class MapActivity :AppCompatActivity(){
-    override fun onCreate(saveInstanceState: Bundle?) {
+class MapActivity :AppCompatActivity(), OnMapReadyCallback{
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMainBinding
+    lateinit var dbHelper: PlaceHelper
+    lateinit var  database: SQLiteDatabase
+
+    override fun onCreate(saveInstanceState: Bundle?){
         super.onCreate(saveInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
         setContentView(R.layout.activity_map)
 
+        dbHelper = PlaceHelper(this, "place.db", null, 1);
+        database = dbHelper.readableDatabase
+
+        val mapFragment: SupportMapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val marker = LatLng(80.241615, 128.695587)
+        mMap.addMarker(MarkerOptions().position(marker).title("marker title"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(marker))
     }
 }
