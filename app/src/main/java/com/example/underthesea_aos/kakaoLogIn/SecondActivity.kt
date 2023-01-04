78eef2b302aefb3c574b4928b4459f0e55ac11a9
wyoung163 <20201014@sungshin.ac.kr>
2023-01-04 23:32:47 +0900

    feat: add kakao social logout and withdrawal temporarily

---
+++ app/src/main/java/com/example/underthesea_aos/kakaoLogIn/SecondActivity.kt
@@ -0,0 +1,47 @@
+package com.example.underthesea_aos.kakaoLogIn
+
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.example.underthesea_aos.R
+import com.kakao.sdk.user.UserApiClient
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        val kakao_logout_button = findViewById<Button>(R.id.kakao_logout_button) // 로그인 버튼
+
+        kakao_logout_button.setOnClickListener {
+            UserApiClient.instance.logout { error ->
+                if (error != null) {
+                    Toast.makeText(this, "로그아웃 실패 $error", Toast.LENGTH_SHORT).show()
+                }else {
+                    Toast.makeText(this, "로그아웃 성공", Toast.LENGTH_SHORT).show()
+                }
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent.addFlags(FLAG_ACTIVITY_CLEAR_TOP))
+                finish()
+            }
+        }
+
+        val kakao_unlink_button = findViewById<Button>(R.id.kakao_unlink_button) // 로그인 버튼
+
+        kakao_unlink_button.setOnClickListener {
+            UserApiClient.instance.unlink { error ->
+                if (error != null) {
+                    Toast.makeText(this, "회원 탈퇴 실패 $error", Toast.LENGTH_SHORT).show()
+                }else {
+                    Toast.makeText(this, "회원 탈퇴 성공", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent.addFlags(FLAG_ACTIVITY_CLEAR_TOP))
+                    finish()
+                }
+            }
+        }
+    }
+}
