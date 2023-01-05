b1a90877ae08a2d37e5edb53e3115c4bc6b28316
wyoung163 <20201014@sungshin.ac.kr>
2023-01-05 11:15:22 +0900

    feat: add textview for id and nickname of a user logged in

--- app/src/main/java/com/example/underthesea_aos/kakaoLogIn/SecondActivity.kt
+++ app/src/main/java/com/example/underthesea_aos/kakaoLogIn/SecondActivity.kt
@@ -5,14 +5,25 @@ import android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import com.example.underthesea_aos.R
 import com.kakao.sdk.user.UserApiClient
+import com.kakao.usermgmt.StringSet.id
+import com.kakao.usermgmt.StringSet.nickname
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val nickname = findViewById<TextView>(R.id.nickname) // 로그인 버튼
+        val id = findViewById<TextView>(R.id.id)
+
+        UserApiClient.instance.me { user, error ->
+            id.text = "회원번호: ${user?.id}"
+            nickname.text = "닉네임: ${user?.kakaoAccount?.profile?.nickname}"
+        }
 
         val kakao_logout_button = findViewById<Button>(R.id.kakao_logout_button) // 로그인 버튼
 
