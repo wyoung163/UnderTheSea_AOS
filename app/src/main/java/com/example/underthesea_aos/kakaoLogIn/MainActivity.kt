a12c72f881a63c71d3a39dc82eb150ec7d379666
wyoung163 <20201014@sungshin.ac.kr>
2023-01-06 15:09:05 +0900

    feat: fix codes to send token to backend

--- app/src/main/java/com/example/underthesea_aos/kakaoLogIn/MainActivity.kt
+++ app/src/main/java/com/example/underthesea_aos/kakaoLogIn/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.example.underthesea_aos.kakaoLogIn
 
+import android.content.ContentValues
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.underthesea_aos.R
+import com.example.underthesea_aos.retrofit.RetrofitBuilder
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.AuthErrorCause.*
+import com.kakao.sdk.common.util.Utility
 import com.kakao.sdk.user.UserApiClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +23,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kakaologin)
 
+        /*
         // 로그인 정보 확인
         UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
             if (error != null) {
@@ -28,7 +36,34 @@ class MainActivity : AppCompatActivity() {
                 finish()
             }
         }
+         */
+
+        //백엔드와의 통신 성공 or 실패
+        fun Login(token: KakaoToken){
+            val call = RetrofitBuilder.api.getKakaoLoginResponse(token)
+            //비동기 방식의 통신
+            call.enqueue(object : Callback<String>{
+                //통신 성공
+                override fun onResponse(call: Call<String>, response: Response<String>) {
+                    //응답 성공
+                    if(response.isSuccessful()){
+                        Log.d("Response: ", response.body().toString())
+                    }
+                    //응답 실패
+                    else{
+                        Log.d("Response: ", "failure")
+                    }
+                }
+                //통신 실패
+                override fun onFailure(call: Call<String>, t: Throwable) {
+                    Log.d("Connection Failure", t.localizedMessage)
+                }
+            })
+        }
+
+        //accesstoken(, refreshtoken) 발급 과정 성공 or 실패
         val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+            //발급 실패
             if (error != null) {
                 when {
                     error.toString() == AccessDenied.toString() -> {
@@ -60,13 +95,25 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
             }
+            //발급 성공
             else if (token != null) {
+                Log.d(ContentValues.TAG, "token : ${token}")
+
                 Toast.makeText(this, "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this, SecondActivity::class.java)
-                startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
-                finish()
+                //val intent = Intent(this, SecondActivity::class.java)
+                //startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+                //finish()
+
+                val kakaoToken = KakaoToken()
+                kakaoToken.accessToken = token.accessToken.toString()
+                kakaoToken.refreshToken = token.refreshToken.toString()
+                kakaoToken.accessExpiration = token.accessTokenExpiresAt.toString()
+                kakaoToken.refreshExpiration = token.refreshTokenExpiresAt.toString()
+                Login(kakaoToken)
             }
         }
+
+        //로그인 버튼 클릭 동작
         val kakao_login_button = findViewById<ImageButton>(R.id.kakao_login_button) // 로그인 버튼
         kakao_login_button.setOnClickListener {
             if(UserApiClient.instance.isKakaoTalkLoginAvailable(this)){
