a037373ece3e5460ee87085225e8949799740305
jooyyoo <aliceyun9@naver.com>
2023-01-08 23:26:32 +0900

    feature: SignIn

--- app/src/main/java/com/example/underthesea_aos/googleLogin/MainActivity.kt
+++ app/src/main/java/com/example/underthesea_aos/googleLogin/MainActivity.kt
@@ -1,4 +1,30 @@
 package com.example.underthesea_aos.googleLogin
 
-class MainActivity {
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.underthesea_aos.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.firebase.auth.FirebaseAuth
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+
+class MainActivity :AppCompatActivity(){
+    private lateinit var auth : FirebaseAuth
+    private lateinit var googleSignInClient : GoogleSignInClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_googlelogin)
+
+        auth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+    }
+
 }
