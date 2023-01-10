a262640ca18bb5796cfd764531d6ce88c0a62baa
jooyyoo <aliceyun9@naver.com>
2023-01-10 23:25:49 +0900

    feature: logout 구현

--- app/src/main/java/com/example/underthesea_aos/googleLogin/HomeActivity.kt
+++ app/src/main/java/com/example/underthesea_aos/googleLogin/HomeActivity.kt
@@ -1,11 +1,31 @@
 package com.example.underthesea_aos.googleLogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.underthesea_aos.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthSettings
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        auth = FirebaseAuth.getInstance()
+
+        val email = intent.getStringExtra("email")
+        val displayName = intent.getStringExtra("name")
+
+        findViewById<TextView>(R.id.textView).text = email + "\n" + displayName
+
+        findViewById<Button>(R.id.SignOutBtn).setOnClickListener{
+            auth.signOut()
+            startActivity(Intent(this,MainActivity::class.java))
+        }
     }
 }
