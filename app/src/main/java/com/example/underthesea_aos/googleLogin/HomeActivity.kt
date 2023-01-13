208948d507679bc4af2d05473c336d97bbf08028
wyoung163 <20201014@sungshin.ac.kr>
2023-01-13 15:46:05 +0900

    feat: google social login

---
+++ app/src/main/java/com/example/underthesea_aos/googleLogin/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.underthesea_aos.googleLogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.underthesea_aos.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
