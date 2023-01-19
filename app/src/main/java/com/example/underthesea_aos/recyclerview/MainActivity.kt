cb80e7a7109bc84bb5d7780b2f9f3d392e626ef4
jooyyoo <aliceyun9@naver.com>
2023-01-19 23:45:11 +0900

    docs: image for test

--- app/src/main/java/com/example/underthesea_aos/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/underthesea_aos/recyclerview/MainActivity.kt
@@ -3,22 +3,42 @@ package com.example.underthesea_aos.recyclerview
 import android.app.Activity
 import android.os.Bundle
 import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.underthesea_aos.R
+import kotlinx.android.synthetic.main.activity_recyclerview.*
 
-class MainActivity : Activity(){
+class MainActivity : AppCompatActivity(){
+    lateinit var profileAdapter: ListAdapterHorizontal
+    val datas = mutableListOf<ProfileData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recyclerview)
 
-        var list = arrayListOf("test1","test2","test3","test4")
-        var manager = LinerLayoutManager(this, LinearLayoutManager.HORIZONTAL,false)
-        var adapter01 = ListAdapterHorizontal(list)
+        initRecycler()
+    }
 
-        var RecyclerView = recyclerHorizon.apply{
-            adapter = adapter01
-            layoutManager = manager
+    private fun initRecycler(){
+        profileAdapter = ListAdapterHorizontal(this)
+
+        rv_profile.adapter = profileAdapter
+        rv_profile.addItemDecoration(VeritcalItemDecorator(20))
+        rv_profile.addItemDecoration(HorizontalItemDecorator(10))
+
+        datas.apply{
+            add(ProfileData(img = R.drawable.profile1, name = "a", age = 24))
+            add(ProfileData(img = R.drawable.profile2, name = "b", age = 24))
+            add(ProfileData(img = R.drawable.profile3, name = "c", age = 24))
+            add(ProfileData(img = R.drawable.profile4, name = "d", age = 24))
+            add(ProfileData(img = R.drawable.profile5, name = "e", age = 24))
+            add(ProfileData(img = R.drawable.profile6, name = "f", age = 24))
+            add(ProfileData(img = R.drawable.profile7, name = "g", age = 24))
+            add(ProfileData(img = R.drawable.profile8, name = "h", age = 24))
+
+            profileAdapter.datas = datas
+            profileAdapter.notifyDataSetChanged()
         }
     }
 }
