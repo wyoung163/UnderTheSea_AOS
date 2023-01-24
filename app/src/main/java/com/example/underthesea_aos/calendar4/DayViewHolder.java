d87e45526d1d6a73eedba61107c12880264af203
wyoung163 <20201014@sungshin.ac.kr>
2023-01-24 21:07:12 +0900

    feat: add various files related with calendar decoration, dates, and etc.

---
+++ app/src/main/java/com/example/underthesea_aos/calendar4/DayViewHolder.java
@@ -0,0 +1,28 @@
+package com.example.underthesea_aos.calendar4;
+
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.underthesea_aos.R;
+
+public class DayViewHolder extends RecyclerView.ViewHolder {// 요일 입 ViewHolder
+
+    public TextView dayText;
+    public TextView timeText;
+    public LinearLayout mainLL;
+
+    public DayViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+
+        dayText = itemView.findViewById(R.id.dayText);
+        timeText = itemView.findViewById(R.id.timeText);
+        mainLL = itemView.findViewById(R.id.mainLL);
+    }
+
+
+}
