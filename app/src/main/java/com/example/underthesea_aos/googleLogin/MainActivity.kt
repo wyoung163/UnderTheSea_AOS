1d8abd220e74e1f9c33dfe9bda46ae72e0ec3b31
jooyyoo <aliceyun9@naver.com>
2023-02-03 18:58:24 +0900

    idToken 가져오면 되는 문제였답니다 !

--- app/src/main/java/com/example/underthesea_aos/googleLogin/MainActivity.kt
+++ app/src/main/java/com/example/underthesea_aos/googleLogin/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContract
@@ -16,8 +17,12 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.firebase.auth.FirebaseAuth
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.internal.StringResourceValueReader
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.functions.FirebaseFunctions
+import com.google.firebase.functions.FirebaseFunctionsException
+import org.json.JSONObject
 
 class MainActivity :AppCompatActivity(){
     private lateinit var auth : FirebaseAuth
@@ -78,9 +83,48 @@ class MainActivity :AppCompatActivity(){
                 intent.putExtra("email",account.email)
                 intent.putExtra("name",account.displayName)
                 startActivity(intent)
+                Log.d("loginSuccess","login")
             }else{//로그인 실패 시
                 Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    private fun onClickCreateJwt(){
+        val text: String = JSONObject()
+            .put("expireSeconds", 60*60*24)
+            .put("did","my_did")
+            .put("muid","my_muid")
+            .toString()
+
+        val data = hashMapOf(
+            "text" to text
+        )
+
+        FirebaseFunctions.getInstance()
+            .getHttpsCallable("createJwtToken")
+            .call(data)
+            .addOnCompleteListener{ task ->
+                if (task.isSuccessful){
+                    val dataString = task.result?.data?.toString()
+                    Log.d("dataString","dataString")
+                    val dataMap = task.result?.data as? Map<String, Object>;
+                    Log.d("dataMap","dataMap")
+                    if(dataMap != null){
+                        if((dataMap["code"] as? Int) == 0){
+                            val resultMap = dataMap["result"] as? Map<String, Object>
+                            val token = resultMap?.get("token") as? String
+                            val exp = resultMap?.get("exp") as? Int
+                            Log.d("getToken","token= $token\nexp=$exp")
+                        }
+                    }
+                }else{
+                    val e = task.exception
+                    if(e is FirebaseFunctionsException){
+                        val code = e.code
+                        val details = e.details
+                    }
+                }
+            }
     }
 }
--- app/src/main/java/com/example/underthesea_aos/plan/AddActivity.kt
+++ app/src/main/java/com/example/underthesea_aos/plan/AddActivity.kt
@@ -2,6 +2,7 @@ package com.example.underthesea_aos.plan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.underthesea_aos.R
 
 class AddActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
