641d08f385028976d83900dcbba23e0bbb7111b8
wyoung163 <20201014@sungshin.ac.kr>
2023-02-06 22:08:09 +0900

    feat: add saved records view page

---
+++ app/src/main/java/com/example/underthesea_aos/record/MainActivity2.kt
@@ -0,0 +1,12 @@
+package com.example.underthesea_aos.record
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.underthesea_aos.R
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recoed2)
+    }
+}
