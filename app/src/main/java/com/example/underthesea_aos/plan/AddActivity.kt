fdef3cf4310ae03ce01321fe1e1824417a707f02
jooyyoo <aliceyun9@naver.com>
2023-02-14 03:41:29 +0900

    fix: new update for plan

--- app/src/main/java/com/example/underthesea_aos/plan/AddActivity.kt
+++ app/src/main/java/com/example/underthesea_aos/plan/AddActivity.kt
@@ -1,11 +1,35 @@
 package com.example.underthesea_aos.plan
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.example.underthesea_aos.R
+import com.example.underthesea_aos.databinding.ActivityAddBinding
 
 class AddActivity : AppCompatActivity() {
+    lateinit var binding: ActivityAddBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add)
+        binding = ActivityAddBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_add, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId){
+        R.id.menu_add_save -> {
+            val intent = intent
+            intent.putExtra("result", binding.addEditView.text.toString())
+            //인텐트에 엑스트라 데이터를 추가하는 함수 putExtra()
+            setResult(RESULT_OK, intent)
+            finish()
+            true
+        }
+        else -> true
     }
 }
--- app/src/main/java/com/example/underthesea_aos/plan/MainActivity.kt
+++ app/src/main/java/com/example/underthesea_aos/plan/MainActivity.kt
@@ -1,37 +1,4 @@
 package com.example.underthesea_aos.plan
 
-import android.os.Bundle
-import android.os.PersistableBundle
-import androidx.appcompat.app.AppCompatActivity
-import com.example.underthesea_aos.R
-import com.example.underthesea_aos.recyclerview.HorizontalItemDecorator
-import com.example.underthesea_aos.recyclerview.VeritcalItemDecorator
-import kotlinx.android.synthetic.main.activity_plan_preview.*
-
-class MainActivity : AppCompatActivity(){
-    lateinit var planAdapter: PreviewAdapterVertical
-    private val vlist = mutableListOf<PlanData>()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_plan_preview)
-
-        initRecycler()
-    } 
-
-    private fun initRecycler(){
-        planAdapter = PreviewAdapterVertical(this)
-        id_todo.adapter = planAdapter
-        id_todo.addItemDecoration(VeritcalItemDecorator(20))
-        id_todo.addItemDecoration(HorizontalItemDecorator(10))
-
-        vlist.apply{
-            add(PlanData(TodoList = "오늘의 할일"))
-            add(PlanData(TodoList = "오늘의 할일"))
-            add(PlanData(TodoList = "오늘의 할일"))
-
-            planAdapter.vlist = vlist
-            planAdapter.notifyDataSetChanged()
-        }
-    }
+class MainActivity {
 }
