bdab16fbf2cb24253fd2365b71b19dfd007f9c41
jooyyoo <aliceyun9@naver.com>
2023-02-14 01:57:51 +0900

    chores: test out email, name, photo

--- app/src/main/java/com/example/underthesea_aos/googleLogin/MainActivity.kt
+++ app/src/main/java/com/example/underthesea_aos/googleLogin/MainActivity.kt
@@ -25,22 +25,22 @@ import com.google.firebase.functions.FirebaseFunctions
 import com.google.firebase.functions.FirebaseFunctionsException
 import org.json.JSONObject
 
-class MainActivity :AppCompatActivity(){
-    private lateinit var auth : FirebaseAuth
-    private lateinit var googleSignInClient : GoogleSignInClient
+class MainActivity :AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_googlelogin)
 
         auth = FirebaseAuth.getInstance()
-        Log.d("auth: ", auth.toString())
+//        Log.d("auth: ", auth.toString())
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
-        Log.d("gso: ", gso.toString())
+//        Log.d("gso: ", gso.toString())
 
         googleSignInClient = GoogleSignIn.getClient(this, gso)
 
@@ -49,88 +49,53 @@ class MainActivity :AppCompatActivity(){
         }
     }
 
-    private fun signInGoogle(){
+    private fun signInGoogle() {
         val signInIntent = googleSignInClient.signInIntent
         launcher.launch(signInIntent)
     }
 
-    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-        result ->
-                if(result.resultCode == Activity.RESULT_OK){
-                    val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
-                    Log.d("task: ", task.toString())
-                    handleResults(task)
-                }
-    }
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+//                    Log.d("task: ", task.toString())
+                handleResults(task)
+            }
+        }
 
     private fun handleResults(task: Task<GoogleSignInAccount>) {
-        if(task.isSuccessful){
-            val account : GoogleSignInAccount? = task.result
-            Log.d("account: ", account.toString())
-            if(account != null){
+        if (task.isSuccessful) {
+            val account: GoogleSignInAccount? = task.result
+//            Log.d("account: ", account.toString())
+            if (account != null) {
                 updateUI(account)
             }
-        }else{
+        } else {
             Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
         }
     }
 
     private fun updateUI(account: GoogleSignInAccount) {
         val credential = GoogleAuthProvider.getCredential((account).idToken, null)
-        Log.d("idToken: ", (account).idToken.toString())
-        auth.signInWithCredential(credential).addOnCompleteListener{
-            if(it.isSuccessful){//로그인 성공 시
-                val intent : Intent = Intent(this,HomeActivity::class.java)
-                intent.putExtra("email",account.email)
-                intent.putExtra("name",account.displayName)
+//        Log.d("idToken: ", (account).idToken.toString())
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {//로그인 성공 시
+                val intent: Intent = Intent(this, SecondActivity::class.java)
+                intent.putExtra("email", account.email)
+                intent.putExtra("name", account.displayName)
                 startActivity(intent)
-                Log.d("loginSuccess","login")
-            }else{//로그인 실패 시
+//                Log.d("loginSuccess","login")
+            } else {//로그인 실패 시
                 Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
             }
         }
+//        Log.d("email:::", account?.email.toString())
+//        Log.d("name:::", account?.displayName.toString())
+//        Log.d("pic:::", account?.photoUrl.toString())
     }
 
-    private fun GoogleLoginInfo(task: Task<GoogleSignInAccount>){
-        val googleInfo = GoogleInfo()
-        val account : GoogleSignInAccount? = task.result
-    }
-
-    private fun onClickCreateJwt(){
-        val text: String = JSONObject()
-            .put("expireSeconds", 60*60*24)
-            .put("did","my_did")
-            .put("muid","my_muid")
-            .toString()
-
-        val data = hashMapOf(
-            "text" to text
-        )
-
-        FirebaseFunctions.getInstance()
-            .getHttpsCallable("createJwtToken")
-            .call(data)
-            .addOnCompleteListener{ task ->
-                if (task.isSuccessful){
-                    val dataString = task.result?.data?.toString()
-                    Log.d("dataString","dataString")
-                    val dataMap = task.result?.data as? Map<String, Object>;
-                    Log.d("dataMap","dataMap")
-                    if(dataMap != null){
-                        if((dataMap["code"] as? Int) == 0){
-                            val resultMap = dataMap["result"] as? Map<String, Object>
-                            val token = resultMap?.get("token") as? String
-                            val exp = resultMap?.get("exp") as? Int
-                            Log.d("getToken","token= $token\nexp=$exp")
-                        }
-                    }
-                }else{
-                    val e = task.exception
-                    if(e is FirebaseFunctionsException){
-                        val code = e.code
-                        val details = e.details
-                    }
-                }
-            }
-    }
+//    private fun GoogleLoginInfo(task: Task<GoogleSignInAccount>) {
+//        val googleInfo = GoogleInfo()
+//        val account: GoogleSignInAccount? = task.result
+//    }
 }
