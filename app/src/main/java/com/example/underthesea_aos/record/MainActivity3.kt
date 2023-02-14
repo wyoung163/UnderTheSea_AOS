9e7ad72a895c9ef68ae1c9e5dbe0956cea6d3722
wyoung163 <20201014@sungshin.ac.kr>
2023-02-15 00:21:38 +0900

    feat: add listview to show records of the date

---
+++ app/src/main/java/com/example/underthesea_aos/record/MainActivity3.kt
@@ -0,0 +1,12 @@
+package com.example.underthesea_aos.record
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.underthesea_aos.R
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_record3)
+    }
+}
