9b5e9673c2e9d4f13b82b1597ecd1517c12c4be8
wyoung163 <20201014@sungshin.ac.kr>
2023-02-14 23:42:14 +0900

    fix: add scroll and delete plan's textview for a selected item

--- app/src/main/java/com/example/underthesea_aos/record/MainActivity.kt
+++ app/src/main/java/com/example/underthesea_aos/record/MainActivity.kt
@@ -33,7 +33,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_record)
         spinner = findViewById(R.id.spinner)
-        result = findViewById(R.id.txt)
         var data = listOf("aaa", "bbb", "ccc");
         var adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
         spinner.adapter = adapter
@@ -42,8 +41,7 @@ class MainActivity : AppCompatActivity() {
 
         spinner.onItemSelectedListener = object:AdapterView.OnItemSelectedListener{
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                //position은 선택한 아이템의 위치를 넘겨주는 인자입니다.
-                result.text = data.get(position)
+
             }
             override fun onNothingSelected(parent: AdapterView<*>?) {
 
