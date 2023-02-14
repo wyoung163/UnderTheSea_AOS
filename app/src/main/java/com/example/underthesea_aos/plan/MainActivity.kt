76ba5561ecf91023c93812928e49716a2e7bf2c1
jooyyoo <aliceyun9@naver.com>
2023-02-14 16:07:17 +0900

    update: new update for plan

--- app/src/main/java/com/example/underthesea_aos/plan/MainActivity.kt
+++ app/src/main/java/com/example/underthesea_aos/plan/MainActivity.kt
@@ -1,4 +1,34 @@
 package com.example.underthesea_aos.plan
 
-class MainActivity {
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.underthesea_aos.R
+import kotlinx.android.synthetic.main.activity_plan_main.*
+
+var DataList = ArrayList<Data>()
+
+class MainActivity : AppCompatActivity() {
+
+    val requestLauncher : ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()){
+        val resultData = it.data?.getStringExtra("result")
+        println("result data is : "+ resultData.toString())
+        DataList += Data(R.drawable.todo, resultData.toString())
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plan_main)
+        main_recyclerView.layoutManager = LinearLayoutManager(this)
+        main_recyclerView.adapter = MyAdapter()
+
+        main_fab.setOnClickListener(){
+            val intent : Intent = Intent(this, AddActivity::class.java)
+            requestLauncher.launch(intent)
+        }
+    }
 }
