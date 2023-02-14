68b614c720a2e5d4bc3c2d0e735e6c688a1ae47f
wyoung163 <20201014@sungshin.ac.kr>
2023-02-14 23:23:29 +0900

    feat: add retrofit api and request data class for records

--- app/src/main/java/com/example/underthesea_aos/retrofit/API.kt
+++ app/src/main/java/com/example/underthesea_aos/retrofit/API.kt
@@ -1,11 +1,17 @@
 package com.example.underthesea_aos.retrofit
 
 import com.example.underthesea_aos.kakaoLogIn.KakaoToken
+import com.example.underthesea_aos.record.RecordInfo
 import retrofit2.Call
-import retrofit2.http.*
+import retrofit2.http.Body
+import retrofit2.http.POST
 
 public interface API {
     //kakao login
     @POST("kakao")
-    fun getKakaoLoginResponse(@Body token: KakaoToken): Call<String>
+    fun postKakaoLoginResponse(@Body token: KakaoToken): Call<String>
+
+    //post records
+    @POST("records")
+    fun postRecordsResponse(@Body record: RecordInfo): Call<String>
 }
