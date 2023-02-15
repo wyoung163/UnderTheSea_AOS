ab7b438df39bb639fa271248b571993c02ff88fe
jooyyoo <aliceyun9@naver.com>
2023-02-16 01:22:47 +0900

    feature: 저장하기 기능 구현 안됨 onCreateOptionsMenu ..

--- app/src/main/java/com/example/underthesea_aos/plan/AddActivity.kt
+++ app/src/main/java/com/example/underthesea_aos/plan/AddActivity.kt
@@ -3,30 +3,32 @@ package com.example.underthesea_aos.plan
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
+import android.view.MenuInflater
 import android.view.MenuItem
 import com.example.underthesea_aos.R
 import com.example.underthesea_aos.databinding.ActivityAddBinding
 
 class AddActivity : AppCompatActivity() {
     lateinit var binding: ActivityAddBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityAddBinding.inflate(layoutInflater)
+        binding= ActivityAddBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
         menuInflater.inflate(R.menu.menu_add, menu)
-        return super.onCreateOptionsMenu(menu)
+        return true
     }
-    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId){
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when(item.itemId){
         R.id.menu_add_save -> {
             val intent = intent
             intent.putExtra("result", binding.addEditView.text.toString())
-            //인텐트에 엑스트라 데이터를 추가하는 함수 putExtra()
-            setResult(RESULT_OK, intent)
+            setResult(Activity.RESULT_OK, intent)
             finish()
             true
         }
