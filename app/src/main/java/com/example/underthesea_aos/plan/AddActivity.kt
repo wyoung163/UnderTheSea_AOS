bea264d9e35334c0d417aed3ae87cbc499326677
jooyyoo <aliceyun9@naver.com>
2023-02-17 20:17:46 +0900

    feature: plan 페이지 만들기

--- app/src/main/java/com/example/underthesea_aos/plan/AddActivity.kt
+++ app/src/main/java/com/example/underthesea_aos/plan/AddActivity.kt
@@ -1,37 +1,24 @@
 package com.example.underthesea_aos.plan
 
-import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import com.example.underthesea_aos.R
-import com.example.underthesea_aos.databinding.ActivityAddBinding
+import kotlinx.android.synthetic.main.activity_add.*
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class AddActivity : AppCompatActivity() {
-    lateinit var binding: ActivityAddBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityAddBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_add)
+
+        showDate()
+
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        super.onCreateOptionsMenu(menu)
-        menuInflater.inflate(R.menu.menu_add, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean = when(item.itemId){
-        R.id.menu_add_save -> {
-            val intent = intent
-            intent.putExtra("result", binding.addEditView.text.toString())
-            setResult(Activity.RESULT_OK, intent)
-            finish()
-            true
-        }
-        else -> true
+    fun showDate(){
+        val date = Date()
+        val formatType = SimpleDateFormat("yyyy-MM-dd")
+        dateTextView.text = formatType.format(date)
     }
 }
