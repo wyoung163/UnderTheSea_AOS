517333760cf2e75ca93f943792e51961239cafe7
jooyyoo <aliceyun9@naver.com>
2023-02-18 23:36:01 +0900

    fix: padding 간격

--- app/src/main/java/com/example/underthesea_aos/plan/AddActivity.kt
+++ app/src/main/java/com/example/underthesea_aos/plan/AddActivity.kt
@@ -3,14 +3,14 @@ package com.example.underthesea_aos.plan
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.underthesea_aos.R
-import kotlinx.android.synthetic.main.activity_add.*
+import kotlinx.android.synthetic.main.activity_plan_add.*
 import java.text.SimpleDateFormat
 import java.util.Date
 
 class AddActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add)
+        setContentView(R.layout.activity_plan_add)
 
         showDate()
 
