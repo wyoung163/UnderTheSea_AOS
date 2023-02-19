c1ccbf3b50caf1a2e8c3c1273ba1068754ef1d02
jooyyoo <aliceyun9@naver.com>
2023-02-19 23:43:19 +0900

    feature: add Data function

--- app/src/main/java/com/example/underthesea_aos/plan/AddActivity.kt
+++ app/src/main/java/com/example/underthesea_aos/plan/AddActivity.kt
@@ -4,15 +4,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.underthesea_aos.R
 import kotlinx.android.synthetic.main.activity_plan_add.*
+import kotlinx.android.synthetic.main.activity_plan_recyclerview.*
+import androidx.recyclerview.widget.RecyclerView
+import com.example.underthesea_aos.recyclerview.HorizontalItemDecorator
+import com.example.underthesea_aos.recyclerview.VeritcalItemDecorator
 import java.text.SimpleDateFormat
 import java.util.Date
 
 class AddActivity : AppCompatActivity() {
+    lateinit var planAdapter: PlanAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_plan_add)
 
         showDate()
+        planadd()
 
     }
 
@@ -20,5 +26,13 @@ class AddActivity : AppCompatActivity() {
         val date = Date()
         val formatType = SimpleDateFormat("yyyy-MM-dd")
         dateTextView.text = formatType.format(date)
+    }
+
+    fun planadd(){
+        planAdapter = PlanAdapter(this)
+
+        imageview_plan_recycler.adapter = planAdapter
+        imageview_plan_recycler.addItemDecoration(VeritcalItemDecorator(20))
+        imageview_plan_recycler.addItemDecoration(HorizontalItemDecorator(10))
     }
 }
