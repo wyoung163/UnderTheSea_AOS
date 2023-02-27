a5a832eee30e03a16030736500043b8394737eb4
wyoung163 <20201014@sungshin.ac.kr>
2023-02-27 23:15:22 +0900

    docs: add data models

--- app/src/main/java/com/example/underthesea_aos/plan/GetPlanRes.kt
+++ app/src/main/java/com/example/underthesea_aos/plan/GetPlanRes.kt
@@ -1,31 +1,7 @@
 package com.example.underthesea_aos.plan
 
-import java.time.LocalDate
-import java.util.*
-
 class GetPlanRes {
-    var planId: Long? = null
-        get() { return field }
-        set(value) { field = value }
-    var userId: Long? = null
-        get() { return field }
-        set(value) { field = value }
-    var freindId: Long? = null
-        get() { return field }
-        set(value) { field = value }
-    var content: String? = null
-        get() { return field }
-        set(value) { field = value }
-    var date: LocalDate? = null
-        get() { return field }
-        set(value) { field = value }
-    var status: String? = null
-        get() { return field }
-        set(value) { field = value }
-    var created_at: Date? = null
-        get() { return field }
-        set(value) { field = value }
-    var updated_at: Date? = null
+    var plans: List<Plan> ?= null
         get() { return field }
         set(value) { field = value }
 }
