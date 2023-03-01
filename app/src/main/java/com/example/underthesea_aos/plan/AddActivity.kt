a0686cace40af03deb4e889bdaf4c341d144b22c
jooyyoo <aliceyun9@naver.com>
2023-03-01 20:53:07 +0900

    docs: 주석 처리 및 import

--- app/src/main/java/com/example/underthesea_aos/plan/AddActivity.kt
+++ app/src/main/java/com/example/underthesea_aos/plan/AddActivity.kt
@@ -1,19 +1,28 @@
 package com.example.underthesea_aos.plan
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.example.underthesea_aos.R
 import kotlinx.android.synthetic.main.activity_plan_add.*
 import kotlinx.android.synthetic.main.activity_plan_recyclerview.*
 import androidx.recyclerview.widget.RecyclerView
+import com.example.underthesea_aos.databinding.ActivityPlanPreivewRecyclerviewBinding
 import com.example.underthesea_aos.recyclerview.HorizontalItemDecorator
 import com.example.underthesea_aos.recyclerview.VeritcalItemDecorator
 import java.text.SimpleDateFormat
 import java.util.Date
 
+/*
+     계획 상세 페이지
+ */
+
 class AddActivity : AppCompatActivity() {
     lateinit var planAdapter: PlanAdapter
     private val dataSet = mutableListOf<RecommendationData>()
+    lateinit var binding : ActivityPlanPreivewRecyclerviewBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +30,6 @@ class AddActivity : AppCompatActivity() {
 
         showDate()
         initRecycler()
-
     }
 
     private fun showDate(){
@@ -46,4 +54,5 @@ class AddActivity : AppCompatActivity() {
         planAdapter.dataSet = dataSet
         planAdapter.notifyDataSetChanged()
     }
+
 }
--- app/src/main/java/com/example/underthesea_aos/plan/MainActivity.kt
+++ app/src/main/java/com/example/underthesea_aos/plan/MainActivity.kt
@@ -11,49 +11,57 @@ import com.example.underthesea_aos.R
 import com.example.underthesea_aos.databinding.ActivityMainBinding
 import com.example.underthesea_aos.databinding.ActivityPlanMainBinding
 import kotlinx.android.synthetic.main.activity_plan_main.*
+/*
+  계획 조회 페이지
+ */
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityPlanMainBinding
-    private var datas : MutableList<String>? = null
+    private var datas: MutableList<String>? = null
     lateinit var adapter: MyAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPlanMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+
+        //ActivityResultLauncher 생성
         val requestLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(
-            ActivityResultContracts.StartActivityForResult())
+            ActivityResultContracts.StartActivityForResult()
+        )
+        //contract -> contract는 ActivityResultLauncher로 실행될 요청을 처리하는 역할
         {
-            it.data!!.getStringExtra("result")?.let{
+            it.data!!.getStringExtra("result")?.let {
                 datas?.add(it)
                 adapter.notifyDataSetChanged()
             }
         }
 
-        binding.addBtn.setOnClickListener{
+        binding.addBtn.setOnClickListener {
             val intent = Intent(this, AddActivity::class.java)
-            requestLauncher.launch(intent)
-        }
-        
-        datas = savedInstanceState?.let{
-            it.getStringArrayList("datas")?.toMutableList()
-        }?: let{
-            mutableListOf<String>()
-        }
-        
-        val layoutManager = LinearLayoutManager(this)
-        binding.mainRecyclerView.layoutManager=layoutManager
-        adapter=MyAdapter(datas)
-        binding.mainRecyclerView.adapter=adapter
-        binding.mainRecyclerView.addItemDecoration(
-            DividerItemDecoration(this,LinearLayoutManager.VERTICAL)
-        )
-    }
+            requestLauncher.launch(intent)//ActivityResultLauncher 실행행        }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putStringArrayList("datas",ArrayList(datas))
+            datas = savedInstanceState?.let {
+                it.getStringArrayList("datas")?.toMutableList()
+            } ?: let {
+                mutableListOf<String>()
+            }
+
+            val layoutManager = LinearLayoutManager(this)
+            binding.mainRecyclerView.layoutManager = layoutManager
+            adapter = MyAdapter(datas)
+            binding.mainRecyclerView.adapter = adapter
+            binding.mainRecyclerView.addItemDecoration(
+                DividerItemDecoration(this, LinearLayoutManager.VERTICAL)
+            )
+        }
+
+        fun onSaveInstanceState(outState: Bundle) {
+            super.onSaveInstanceState(outState)
+            outState.putStringArrayList("datas", ArrayList(datas))
+        }
     }
 }
