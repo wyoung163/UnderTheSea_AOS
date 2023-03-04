90fb7f833ed050d9afa484a05ab33a1c6eb0b083
wyoung163 <20201014@sungshin.ac.kr>
2023-03-04 19:10:00 +0900

    feat: add main page

---
+++ app/src/main/java/com/example/underthesea_aos/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.underthesea_aos.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.underthesea_aos.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
