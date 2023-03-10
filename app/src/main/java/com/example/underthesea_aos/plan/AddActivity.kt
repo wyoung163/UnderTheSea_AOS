652acc1940e3d85ca14c38508471f09c170bbbe9
wyoung163 <20201014@sungshin.ac.kr>
2023-03-10 23:27:33 +0900

    fix: add friend spinner

--- app/src/main/java/com/example/underthesea_aos/plan/AddActivity.kt
+++ app/src/main/java/com/example/underthesea_aos/plan/AddActivity.kt
@@ -1,21 +1,23 @@
 package com.example.underthesea_aos.plan
 
-import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.underthesea_aos.BaseResponse.BaseResponse
 import com.example.underthesea_aos.R
-import kotlinx.android.synthetic.main.activity_plan_add.*
-import kotlinx.android.synthetic.main.activity_plan_recyclerview.*
-import androidx.recyclerview.widget.RecyclerView
-import com.example.underthesea_aos.databinding.ActivityPlanAddBinding
-import com.example.underthesea_aos.databinding.ActivityPlanPreviewRecyclerviewBinding
 import com.example.underthesea_aos.recyclerview.HorizontalItemDecorator
 import com.example.underthesea_aos.recyclerview.VeritcalItemDecorator
-import java.text.SimpleDateFormat
-import java.util.Date
+import com.example.underthesea_aos.retrofit.RetrofitBuilder
+import com.example.underthesea_aos.user.GetFriendRes
+import kotlinx.android.synthetic.main.activity_plan_add.*
+import retrofit2.Call
+import retrofit2.Response
 
 /*
      계획 상세 페이지
@@ -24,21 +26,131 @@ import java.util.Date
 class AddActivity : AppCompatActivity() {
     lateinit var planAdapter: PlanAdapter
     private val dataSet = mutableListOf<RecommendationData>()
-    lateinit var binding : ActivityPlanAddBinding
+    var strDate = ""
+    lateinit var spinner: Spinner
+    var friendNames =  ArrayList<String>()
+    var friendIdx =  ArrayList<Long>()
+    var friendId = 0.toLong()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_plan_add)
-        showDate()
+
+        spinner = findViewById(R.id.spinner)
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_checked, friendNames)
+        spinner.adapter = adapter
+        adapter.notifyDataSetChanged()
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                adapterView: AdapterView<*>?,
+                view: View,
+                i: Int,
+                l: Long
+            ) {
+                friendId = friendIdx[i]
+                Log.d("friend: ", friendId.toString())
+            }
+
+            override fun onNothingSelected(adapterView: AdapterView<*>?) {}
+        }
+
+        if (intent.hasExtra("date")) {  //date라는 키값을 가진 intent가 정보를 가지고 있다면 실행
+            // date라는 id의 textview의 문구를 date라는 키값을 가진 intent의 정보로 변경
+            strDate = intent.getStringExtra("date").toString()
+            Log.d("date", intent.getStringExtra("date").toString())
+            date.text = strDate
+            intent.putExtra("date", strDate)
+        }
+
         initRecycler()
-        back()
-        cancel()
+
+        //save 저장하기 버튼
+        val planInfo = Plan()
+        save_button.setOnClickListener{
+            PostPlan(planInfo)
+
+            val intent3 = Intent(this, MainActivity::class.java)
+            startActivity(intent3)
+            finish()
+            Toast.makeText(this, "저장이 완료되었습니다", Toast.LENGTH_SHORT).show()
+        }
+
+        //뒤로 가기 버튼
+        back_btn.setOnClickListener{
+            val intent1 = Intent(this, MainActivity::class.java)
+            startActivity(intent1)
+        }
+
+        //cancel 버튼
+        cancel_button.setOnClickListener{
+            val intent2 = Intent(this,MainActivity::class.java)
+            startActivity(intent2)
+        }
+
+        //친구 목록을 보여주는 spinner
+        GetFriends()
     }
 
-    private fun showDate(){
-        val date = Date()
-        val formatType = SimpleDateFormat("yyyy-MM-dd")
-        dateTextView.text = formatType.format(date)
+    private fun GetFriends(){
+        val call = RetrofitBuilder().retrofit().getFriendResponse()
+        //비동기 방식의 통신
+        call.enqueue(object : retrofit2.Callback<BaseResponse<GetFriendRes>> {
+            //통신 성공
+            override fun onResponse(
+                call: Call<BaseResponse<GetFriendRes>>,
+                response: Response<BaseResponse<GetFriendRes>>
+            ) {
+                if(response.isSuccessful()){
+                    Log.d("Response: ", response.body().toString())
+                    Log.d("length: ", response.body()?.result?.friend?.size.toString())
+                    if(response.body()?.result?.friend?.size != null){
+                        for(i in 0..response.body()!!.result!!.friend!!.size-1) {
+                            friendNames.add(response.body()!!.result!!.friend!![i].nickname.toString())
+                            friendIdx.add(response.body()!!.result!!.friend!![i].userId!!)
+                            //Log.d("friend", friendIdx[0].toString())
+                        }
+                    }
+                }
+                //응답 실패
+                else{
+                    Log.d("Response: ", "failure")
+                }
+            }
+
+            override fun onFailure(call: Call<BaseResponse<GetFriendRes>>, t: Throwable) {
+                Log.d("Connection Failure", t.localizedMessage)
+            }
+        })
+    }
+
+    private fun PostPlan(plan: Plan){
+        plan.title = title_plan.text.toString()
+        plan.content = contents_memo.text.toString()
+        plan.date = strDate
+        plan.friend = friendId
+
+        val call = RetrofitBuilder().retrofit().postPlanResponse(plan)
+        //비동기 방식의 통신
+        call.enqueue(object : retrofit2.Callback<BaseResponse<Long>> {
+            //통신 성공
+            override fun onResponse(
+                call: Call<BaseResponse<Long>>,
+                response: Response<BaseResponse<Long>>
+            ) {
+                if(response.isSuccessful()){
+                    Log.d("Response: ", response.body().toString())
+                }
+                //응답 실패
+                else{
+                    Log.d("Response: ", "failure")
+                }
+            }
+
+            override fun onFailure(call: Call<BaseResponse<Long>>, t: Throwable) {
+                Log.d("Connection Failure", t.localizedMessage)
+            }
+        })
     }
 
     private fun initRecycler(){
@@ -57,23 +169,4 @@ class AddActivity : AppCompatActivity() {
         planAdapter.dataSet = dataSet
         planAdapter.notifyDataSetChanged()
     }
-
-    //preview 페이지 돌아가기
-    private fun back(){
-        binding.backBtn.setOnClickListener{
-            val intent1 = Intent(this, MainActivity::class.java)
-            startActivity(intent1)
-        }
-    }
-
-    //cancle
-    private fun cancel(){
-        binding.cancelButton.setOnClickListener{
-           val back = Intent(this, MainActivity::class.java)
-            startActivity(back)
-        }
-    }
-
-    //save
-
 }
